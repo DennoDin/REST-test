f851c3067671d1f1602cc777dde01d890d71a7b9
Dustin Tran <dustin.andy.tran@gmail.com>
2021-06-29 17:38:59 +0900

    FIX: port name and procfile

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.status(200).send("Hello World!")
 })
 
-app.listen(process.env.port || 3000, () => console.log("Server running!"))
+app.listen(process.env.PORT || 3000, () => console.log("Server running!"))
