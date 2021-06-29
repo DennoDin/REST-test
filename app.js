23e93110e672442670bd06f9fe1497f16fa26ec9
Dustin Tran <dustin.andy.tran@gmail.com>
2021-06-29 17:29:39 +0900

    WIP: test get endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-    res.send("Hello World!")
+    res.status(200).send("Hello World!")
 })
 
 app.listen(process.env.port || 3000, () => console.log("Server running!"))
