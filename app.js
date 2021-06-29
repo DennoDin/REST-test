93d2cfb85f526a5a812319814d6464b4a9024029
Dustin Tran <dustin.andy.tran@gmail.com>
2021-06-29 17:22:56 +0900

    WIP: hello world get test

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
 app.listen(process.env.port || 3000, () => console.log("Server running!"))
